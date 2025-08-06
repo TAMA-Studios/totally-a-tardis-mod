ddbdc826cc736ce5c66b72d8a36997a86aaa2815
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2025-08-05 21:16:33 -0500

    first commit. you will see more but from jarvis :)

---
+++ src/client/java/com/tdjs/tatm/TatmDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tdjs.tatm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TatmDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
